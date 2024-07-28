0c1d73369b8822f435c153d8f3b6cb8a46005a3a
Asrai04 <a.saborio.1@estudiantec.cr>
2024-07-27 19:04:54 -0600

    Iniciar servidor

--- Mesageria/Program.cs
+++ Mesageria/Program.cs
@@ -1,13 +1,35 @@
-﻿internal class program()
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;
+
+internal class program()
 {
     public static void Main(string[] args)
     {
         ListenAsync();
         Console.ReadKey();
     }
-#hola
     async static void ListenAsync()
     {
-#prueba_de_git
+        HttpListener listener = new();
+        listener.Prefixes.Add("http://localhost:50001/");
+        listener.Start();
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            string? url = context.Request.RawUrl;
+            int CReturn = (int)HttpStatusCode.OK;
+            string mensaje = "nice";
+
+            context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(mensaje);
+            context.Response.StatusCode = CReturn;
+            using (Stream s = context.Response.OutputStream)
+            using (StreamWriter writer = new(s))
+                await writer.WriteAsync(mensaje);
+
+        }
+
     }
 }
